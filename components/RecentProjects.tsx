b9ad5e18ec9e9854204feb06200e3665f7694ddc
harshinWork <harshinworkid@gmail.com>
2025-07-27 21:57:49 +0530

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -20,18 +20,19 @@ const RecentProjects = () => {
           >
             <PinContainer
               title="/ui.aceternity.com"
-              href="https://twitter.com/mannupaaji"
+              href="https://"
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
                   style={{ backgroundColor: "#13162D" }}
                 >
-                  <img src={/bg.png} alt="bgimg" />
-                  
+                  <img src="/bg.png" alt="bgimg" />
                 </div>
                 <img
-                  src={`dheeraj-portfolio${item.img}`}
+                  src={item.img}
+                  // src={`dheeraj-portfolio${item.img}`}
+
                   alt="cover"
                   className="z-10 absolute bottom-0"
                 />
